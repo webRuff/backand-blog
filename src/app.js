03a32679512ad007183c0a9e912ef10eb7645f73
webRuff <bogatovsa87@mail.ru>
2020-04-23 11:43:25 +0300

    В проект добавлен dotenv для работы с конфигами

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
 
-const port = 7777;
+const root = path.join.bind(this, __dirname, '../');
+dotenv.config({ path: root('.env') });
+
 const app = express();
 
 app.get('/',(request, response)=> {
     response.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Express server run http://${process.env.HOST}:${process.env.PORT}`);
 });

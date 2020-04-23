6d0ea6573afee0887fcdf764b9583ff4bba9fbcf
webRuff <bogatovsa87@mail.ru>
2020-04-23 13:51:00 +0300

    Добавлен enpoint авторизации и добавлен пакет body-parser

--- src/app.js
+++ src/app.js
@@ -1,19 +1,82 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-import cors from 'cors'
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const root = path.join.bind(this, __dirname, '../');
 dotenv.config({ path: root('.env') });
 
+// front-end data
+
+const users = [
+  {name: 'Alex', login: 'alex', password: '123'},
+  {name: 'Vera', login: 'vera', password: '222'},
+];
+
+const posts = [
+  {
+      id: 1,
+      header: "Кобаяси Исса — Я наказал ребенка",
+      content:
+          "Я наказал ребенка,\n" +
+          "Но привязал его к дереву там,\n" +
+          "Где дует прохладный ветер." +
+          "Я наказал ребенка,\n" +
+          "Но привязал его к дереву там,\n" +
+          "Где дует прохладный ветер." +
+          "Я наказал ребенка,\n" +
+          "Но привязал его к дереву там,\n" +
+          "Где дует прохладный ветер." +
+          "Я наказал ребенка,\n" +
+          "Но привязал его к дереву там,\n" +
+          "Где дует прохладный ветер." +
+          "Я наказал ребенка,\n" +
+          "Но привязал его к дереву там,\n" +
+          "Где дует прохладный ветер." +
+          "Я наказал ребенка,\n" +
+          "Но привязал его к дереву там,\n" +
+          "Где дует прохладный ветер.",
+  },
+  {
+      id: 2,
+      header: "Мацуо Басё — Луна проплыла",
+      content:
+          "Луна проплыла,\n" + "Ветви оцепенели\n" + "В блестках дождевых.",
+  },
+  {
+      id: 3,
+      header: "Мацуо Басё — Прощайте, вишни",
+      content:
+          "Прощайте, вишни!\n" + "Цветенье ваше мой путь\n" + "Теплом согреет.",
+  },
+  {
+      id: 4,
+      header: "Мацуо Басё — Совсем исхудал",
+      content: "Совсем исхудал,\n" + "И волосы отросли.\n" + "Долгие дожди.",
+  },
+]
+
 const app = express();
 app.use(cors());
+app.use(bodyParser.json);
 
-app.get('/',(request, response)=> {
-    response.send('Hello World!');
+/** 
+*Endpoints
+*/
+
+app.post('/auth',(request, response)=> {
+  const {login, password} = request.body;
+  const user = users.find((user)=> user.login === login && user.password === password);
+
+  if (!user) {
+    response.status(401).json({status: false, message: 'Incorrect login or password'});
+  }
+
+    response.json({status: true, user});
 });
 
-app.get('*',(request, response)=> {
+app.use('*',(request, response)=> {
   response.status(404).send('Endpoint not found on server');
 });
 
